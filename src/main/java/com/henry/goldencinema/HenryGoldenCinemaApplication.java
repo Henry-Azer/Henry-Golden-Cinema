402248c95916db546bd1156612f594e856ba5e87
Henry-Azer <henaryazer@outlook.com>
2022-03-01 19:38:50 +0200

    Initial commit

---
+++ src/main/java/com/henry/goldencinema/HenryGoldenCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.henry.goldencinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HenryGoldenCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HenryGoldenCinemaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henry/goldencinema/HenryGoldenCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.goldencinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HenryGoldenCinemaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
