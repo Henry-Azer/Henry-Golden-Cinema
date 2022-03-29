58f2711d91bd95d913ac671da5a8ea9311cc47b6
Henry-Azer <henaryazer@outlook.com>
2022-03-29 13:12:46 +0200

    Add component annotation for shows scheduler

--- src/main/java/henry/goldencinema/schedule/ShowsScheduler.java
+++ src/main/java/henry/goldencinema/schedule/ShowsScheduler.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.*;
 
+@Component
 @Configuration
 @EnableScheduling
 public class ShowsScheduler {
